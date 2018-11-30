e33b20f7da12b668b271eaf77925fffcaeb41cca
Ilham Dedi <muhammadilhamdedi@gmail.com>
2018-11-30 23:54:46 +0700

    try any in api

--- internal/api/events.go
+++ internal/api/events.go
@@ -1,24 +1,36 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
+	"github.com/golang/protobuf/ptypes/any"
 	"github.com/milhamdedi/excite-go/model"
 )
 
-// Get all events
-func getEvents(w http.ResponseWriter, r *http.Request) {
+// getEvents all events
+func getEvents(id string) any.Any {
+	events := []model.Event{}
+	toRead := []string{}
+	if id != "" {
+		toRead = append(toRead, id)
+	} else {
+		toRead = append(toRead, "codejam", "soc")
+	}
 
-}
-
-// Get specific event
-func getEvent(w http.ResponseWriter, r *http.Request) {
-
+	for _, id := range toRead {
+		event, err := model.ReadCard(id)
+		if err != nil {
+			return events
+		}
+		events = append(events, *event)
+	}
+	return events
 }
 
 // Create a New Event
-func createEvents(w http.ResponseWriter, r *http.Request) {
+func createEvent(id string, r map[string]any.Any) {
 
 }
 
@@ -34,8 +46,31 @@ func deleteEvents(w http.ResponseWriter, r *http.Request) {
 
 // SaveAndReadCard test util
 func SaveAndReadCard() {
-	e1 := &model.Event{Name: "Testevent", Description: []byte("Event for new Go Developers.")}
+	e1 := model.Event{Name: "Testevent", Description: []byte("Event for new Go Developers.")}
 	e1.SaveCard()
 	e2, _ := model.ReadCard("Testevent")
 	fmt.Println(string(e2.Description))
+}
+
+// HandleEvents is API entrypoint
+func HandleEvents(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	id := r.URL.Path[len("/api/events"):]
+
+	var response map[string]*any.Any
+
+	switch r.Method {
+	case "GET":
+		response["events"] = getEvents(id)
+	case "POST":
+		createEvent(id, map[string]*any.Any)
+	case "PUT":
+		updateEvent(id, map[string]*any.Any)
+	case "DELETE":
+		deleteEvent(id)
+	default:
+		response["message"] = "Action not recognized"
+	}
+
+	json.NewEncoder(w).Encode(response)
 }
--- internal/api/init.go
+++ internal/api/init.go
@@ -8,14 +8,22 @@ import (
 
 // HandleFunc is API entrypoint
 func HandleFunc(w http.ResponseWriter, r *http.Request) {
+	// to do: design better routing
 	path := r.URL.Path[len("/api/"):]
-	fmt.Fprintf(w, "Hello, World. Hello, %s!", path)
-	switch path[:strings.Index(path, "/")] {
+
+	fmt.Fprintf(w, "Hello, %s!\n", path)
+	if strings.Index(path, "/") < 0 {
+		return
+	}
+
+	resource := path[:strings.Index(path, "/")]
+
+	switch resource {
 	case "events":
-		fmt.Println("one" + path[:strings.Index(path, "/")])
+		fmt.Fprintf(w, "one "+path[:strings.Index(path, "/")]+r.Method)
 	case "organizations":
-		fmt.Println("two" + path[:strings.Index(path, "/")])
+		fmt.Fprintf(w, "two "+path[:strings.Index(path, "/")])
 	default:
-		fmt.Println("three" + path[:strings.Index(path, "/")])
+		fmt.Fprintf(w, "three "+path[:strings.Index(path, "/")])
 	}
 }
